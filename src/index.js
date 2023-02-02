08add7d654368ff94024e3636c0a5033713f4f4c
evelin parra <evelind451@gmail.com>
2023-02-02 12:40:42 -0300

    mi segundo commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
+//importe las funciones declaradas en validator.js e hice refrerencia en este index.js//
 
-console.log(validator);
+document.getElementById("myBtn").addEventListener("click", function(){//funcion para validar mi boton //
+  const cardNumber = document.getElementById("cardnumber").value;//variable que recibe el numero de la tarjeta//
+  const lastFourDigits = validator.maskify(cardNumber);//para ocultar los ultimos cutro numeros//
+  document.getElementById("cardnumber").value= lastFourDigits;//el valor que se le de a cardnumber se olcuten los ultimos 4 numeros//
+  document.getElementById("message").innerText = '';//insertando espacio vacio para agregar los mensajes //
+  if (validator.isValid(cardNumber)) {
+    //const boleanos=validator.isValid(cardNumber);//si los valores son verdaderos o falsos//
+    //alert(boleanos);
+    document.getElementById("message").innerHTML = "Número de tarjeta válido.";
+    //document.getElementById("result").innerHTML = cardNumber;
+  } else {
+    document.getElementById("message").innerHTML = "Número de tarjeta inválido.";
+    document.getElementById("result").innerHTML = "";
+  }
+});
+
+
+
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,49 @@
 const validator = {
-  // ...
-};
+ 
+  maskify: function (creditCardNumber){      //funcion maskif, como enmascarar x //
+    //console.log(creditCardNumber);
+    //console.log(creditCardNumber.length);
+    if (creditCardNumber.length > 4) {       
+      const hideNumbers = "#".repeat(creditCardNumber.length-4); //constante hide que me oculte los caracteres seleccionados, que me repita ,
+      const showLastFour = creditCardNumber.substring(creditCardNumber.length-4);//la cadena  con .repeat y los remplace esa cadena con showwlastfour.
+      const mask = hideNumbers + showLastFour;
+      //console.log(mask);
+      return mask; 
+
+    } else {
+      return creditCardNumber;
+
+    }
+  },
+  isValid: function (creditCardNumber){
+    let cardStringReverse = '';
+    const splitCardNumber = creditCardNumber.split(""); // esta función (split) convierte un string a array
+    const arrayReverse = splitCardNumber.reverse(); // esta funcion (reverse) reversa el array creado
+    let acum = 0;
+    //console.log(arrayReverse);//npm run start
+    for (let index = 0; index < arrayReverse.length; index++) {
+      let element = Number(arrayReverse[index]);
+      if ((index + 1) % 2 === 0) {
+        element = element * 2;
+        if (element > 9) {
+          const sumanumero = element.toString(); //Convierte el numero mayor a 9 en string (secuencia de caracteres);
+          const stringvector = sumanumero.split("");//sumar los numeros dentnro del vector;
+          element = parseInt(stringvector[0]) + parseInt(stringvector[1]);
+        }
+      }
+      cardStringReverse = cardStringReverse + element;
+      acum = acum + element;
+    }
+    if (acum % 10 === 0) {
+      return true;
+    } 
+    else {
+      return false; 
+          
+    }
+  
+  }
+}
 
 export default validator;
+
